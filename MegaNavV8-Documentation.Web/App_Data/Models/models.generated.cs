1dfeb896a73473537d59257f3e48d4bfaa256176
Aaron Sadler <aaron@aaronsadler.co.uk>
2021-02-17 22:54:12 +0000

    Added base CoderDocs theme

--- MegaNavV8-Documentation.Web/App_Data/Models/models.generated.cs
+++ MegaNavV8-Documentation.Web/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3e3c8db05b0ba5bf")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
